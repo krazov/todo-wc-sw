f45111b25eed5cb6ec32f26bee66eeaa2094ebcb
Krazov <krazov@interia.pl>
2021-02-21 15:15:03 +0100

    Reverting to Promise chain (it’s not async!)

--- javascript/utils/dom.stylesheet-constructor.js
+++ javascript/utils/dom.stylesheet-constructor.js
@@ -1,12 +1,11 @@
 export function stylesheet(style = '') {
     const sheet = new CSSStyleSheet;
 
-    try {
-        sheet.replace(style);
-    } catch (error) {
-        console.warn('Error while creating a stylesheet:', style);
-        console.error(error);
-    }
+    sheet.replace(style)
+        .catch((error) => {
+            console.warn('Error while creating a stylesheet:', style);
+            console.error(error);
+        });
 
     return sheet;
 }

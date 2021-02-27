4a479bf5954b7a0c37fd33c4e62e108419990748
Krazov <krazov@interia.pl>
2021-02-27 18:40:54 +0100

    Fixing list sorting

--- components/todo-list/todo-list.js
+++ components/todo-list/todo-list.js
@@ -9,9 +9,9 @@ const sheet = stylesheet({ url: '/components/todo-list/todo-list.css' });
 
 const compareTodos = (a, b) =>
     a.isDone && !b.isDone
-        ? -1
+        ? 1
         : !a.isDone && b.isDone
-            ? 1
+            ? -1
             : a.id - b.id;
 
 class TodoList extends HTMLElement {

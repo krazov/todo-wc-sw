00a31e2747997ca56feafa4a0bef78eb078591ea
Krazov <krazov@interia.pl>
2021-02-28 15:45:04 +0100

    New type of DB event

--- constants/db.js
+++ constants/db.js
@@ -1,6 +1,7 @@
 export const OPEN_DB = 'OPEN_DB';
 export const DB_INITED = 'DB_INITED';
 export const LIST_UPDATED = 'LIST_UPDATED';
+export const TODOS_FETCH = 'TODOS_FETCH';
 export const TODO_ADD = 'TODO_ADD';
 export const TODO_EDIT = 'TODO_EDIT';
 export const TODO_REMOVE = 'TODO_REMOVE';

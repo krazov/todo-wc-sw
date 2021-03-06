d5248051fdb0e51957748b17522cb8bc09111547
Krazov <krazov@interia.pl>
2021-03-06 18:11:44 +0100

    Adding a missing dependency after moving the code

--- router/route-home.js
+++ router/route-home.js
@@ -1,6 +1,8 @@
 import { EDITED_TODO_SUBMITTED } from '/components/todo-item/todo-item-events.js';
 import { TODO_EDIT } from '/constants/db.js';
 
+import { ServiceWorkerBus } from '/workers/service-worker-bus.js';
+
 import '/components/todo-list/todo-list-component.js';
 import '/components/todo-form/todo-form-component.js';
 
